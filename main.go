36db3d4774a75ba3c44ead176f1a1b8386987477
4cecoder <4cecoder@users.noreply.github.com>
2024-04-15 13:23:14 -0400

    overhaul with event system and rendering system

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/4cecoder/multiplayer/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
@@ -21,8 +29,15 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	r.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
-	log.Println("Server started on :8080")
-	err := http.ListenAndServe(":8080", r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Println("PORT environment variable not set")
+		log.Println("Using default port 8080")
+		port = "8080" // Default port if not specified
+	}
+
+	log.Printf("Server started on :%s", port)
+	err = http.ListenAndServe(":"+port, r)
 	if err != nil {
 		log.Fatal(err)
 		return
